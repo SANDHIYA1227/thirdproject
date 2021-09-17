65795650d717d46d3803e1ce9254e76a39a92233
SANDHIYA1227 <77952109+SANDHIYA1227@users.noreply.github.com>
2021-09-17 01:47:57 -0900

    changed variable value

--- Add.java
+++ Add.java
@@ -2,8 +2,8 @@ class Add
 {
     public static void main(String args[])
     {
-        int a=1;
-        int b=1;
+        int a=10;
+        int b=10;
         int c= a+b;
         System.out.println(c);
     }
